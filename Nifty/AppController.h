6c4ef764ad94f9657d106bc07477a6139ed7dc84
Ben Stolovitz <ben@stolovitz.com>
2011-12-03 21:11:45 -0500

    Accidentally pushed gh-pages to dev.

---
+++ Nifty/AppController.h
@@ -0,0 +1,24 @@
+//
+//  AppController.h
+//  Nifty
+//
+//  Created by Stolovitz on 11/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppController : NSObject {
+    IBOutlet NSTableView *commandOutput;
+    IBOutlet NSTextField *commandInput;
+    IBOutlet NSTextView *debugCommandOutput;
+    NSTask *server;
+    NSFileHandle *stdi;
+    NSFileHandle *stdo;
+}
+
+- (void)handleCommandOutput: (NSNotification *)aNotification;
+- (IBAction)handleCommandInput: (id)sender;
+- (void)handleCommandInput: (id)sender withInput: (NSString *)data;
+
+@end
